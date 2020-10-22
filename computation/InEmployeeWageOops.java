7627f6f31df65168cd79801687b633567ff36049
samrukavya18 <samrukavya18@gmail.com>
2020-10-22 12:36:37 +0530

    WELCOME TO EMPLOYEE WAGE COMPUTATION

---
+++ computation/InEmployeeWageOops.java
@@ -0,0 +1,6 @@
+package com.computation;
+
+public interface InEmployeeWageOops {
+	 public void addCompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth);
+
+}
